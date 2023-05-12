ab593fa9e5f911bdf7da77a5b199ecabb65ac82a
Devin Hansen <hansdevi@isu.edu>
2023-05-12 18:46:01 -0500

    Final commit before final push.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function Header(props){
-
-    return(
-        <div className="header">
-            {props?.showBack?
-                <Link to="/"  id="backCarrot">
-                    <h1>&lt;</h1>
-                </Link>
-                :
-                console.log("Nope")
-            }
-            
-            <h3 id="headerTitle">{props.title}</h3>
-            
+export default function Header(props) {
+  return (
+    <div className="header">
+      {props?.showBack ? (
+        <div className="backCarrotCont">
+          <Link to="/" id="backCarrot">
+            <h1 className="backCarrotCont">&lt;</h1>
+          </Link>
         </div>
-    )
+      ) : (
+        console.log("Nope")
+      )}
+
+      <h3 id="headerTitle">{props.title}</h3>
+    </div>
+  );
 }
